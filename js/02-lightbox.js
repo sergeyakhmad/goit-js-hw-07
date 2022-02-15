c99163ba6d1bf83e90dde674b716b1cebe3027d6
Sergey Akhmadaliev <Sergan86@ukr.net>
2022-02-15 17:50:58 +0200

    add ДЗ№2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+// galleryContainerEl.addEventListener("click", onGalleryContainerClick);
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+populateGallery(galleryItemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+function populateGallery(markup) {
+  galleryContainerEl.innerHTML = markup;
+}
+
+let gallery = new SimpleLightbox(
+  ".gallery a",
+  { captionsData: "alt" },
+  { captionDelay: 250 }
+);
